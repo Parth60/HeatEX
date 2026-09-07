fe0f23cb4ff8a731abc79b6a980197f95a5dd10b
Parth A Yogi <65752335+Parth60@users.noreply.github.com>
2026-09-07 15:51:04 +1000

    Add files via upload

--- backend/app/engine/__init__.py
+++ backend/app/engine/__init__.py
@@ -1,14 +1,52 @@
-from .geometry import ShellTubeGeometry
-from .tube_side import calculate_tube_side
-from .bell_delaware import calculate_bell_delaware, BellDelawareInputs
-from .heat_transfer import lmtd, overall_u_outside_basis, required_area_m2
+from .components import COMPONENTS, component_names, get_component
+from .composition import normalize_composition
+from .engine import ThermoState, evaluate_state, solve_temperature_for_enthalpy
+from .eos import cubic_eos
+from .activity import nrtl_activity_coefficients, uniquac_activity_coefficients
+from .phase_change import (
+    SaturationState,
+    saturation_temperature_c,
+    get_saturation_state,
+    pure_phase_enthalpy_j_kg,
+)
+from .flash import (
+    FlashResult,
+    wilson_k_values,
+    solve_rachford_rice,
+    flash_isothermal,
+)
+from .phase_envelope import temperature_flash_sweep
+from .two_phase_path import build_phase_path
+from .mixture_phase import (
+    PhaseProperties,
+    EquilibriumMixtureState,
+    equilibrium_mixture_state,
+    effective_latent_heat_j_kg,
+)
 
 __all__ = [
-    "ShellTubeGeometry",
-    "calculate_tube_side",
-    "calculate_bell_delaware",
-    "BellDelawareInputs",
-    "lmtd",
-    "overall_u_outside_basis",
-    "required_area_m2",
+    "COMPONENTS",
+    "component_names",
+    "get_component",
+    "normalize_composition",
+    "ThermoState",
+    "evaluate_state",
+    "solve_temperature_for_enthalpy",
+    "cubic_eos",
+    "nrtl_activity_coefficients",
+    "uniquac_activity_coefficients",
+    "SaturationState",
+    "saturation_temperature_c",
+    "get_saturation_state",
+    "pure_phase_enthalpy_j_kg",
+    "FlashResult",
+    "wilson_k_values",
+    "solve_rachford_rice",
+    "flash_isothermal",
+    "temperature_flash_sweep",
+    "build_phase_path",
+    "PhaseProperties",
+    "EquilibriumMixtureState",
+    "equilibrium_mixture_state",
+    "effective_latent_heat_j_kg",
 ]
