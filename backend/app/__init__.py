b576ced632246e6956088e46b5638d9856ebcc5b
Parth A Yogi <65752335+Parth60@users.noreply.github.com>
2026-09-07 15:22:36 +1000

    Add files via upload

---
+++ backend/app/__init__.py
@@ -0,0 +1,18 @@
+from .components import COMPONENTS, component_names, get_component
+from .composition import normalize_composition
+from .engine import ThermoState, evaluate_state, solve_temperature_for_enthalpy
+from .eos import cubic_eos
+from .activity import nrtl_activity_coefficients, uniquac_activity_coefficients
+
+__all__ = [
+    "COMPONENTS",
+    "component_names",
+    "get_component",
+    "normalize_composition",
+    "ThermoState",
+    "evaluate_state",
+    "solve_temperature_for_enthalpy",
+    "cubic_eos",
+    "nrtl_activity_coefficients",
+    "uniquac_activity_coefficients",
+]
